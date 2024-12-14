a4bc97cd1fbe51ae40dc97f4d21f461509724068
Кузичев Александр <pulsarmn@yandex.ru>
2024-12-14 12:00:42 +0300

    add: implement the start_menu() function

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -2,3 +2,17 @@ from aiogram.types import InlineKeyboardButton, KeyboardButton, ReplyKeyboardMar
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
+def start_menu(is_premium: bool = False):
+    kb = InlineKeyboardBuilder()
+
+    kb.row(InlineKeyboardButton(text='Сгенерировать код по запросу', callback_data='generate_text_on_query'),
+           InlineKeyboardButton(text='Сгенерировать случайный код', callback_data='generate_random_text'))
+    kb.row(InlineKeyboardButton(text='Помощь', url='https://telegra.ph/'))
+
+    if not is_premium:
+        kb.row(InlineKeyboardButton(text='Купить подписку', callback_data='sub'))
+
+    kb.row(InlineKeyboardButton(text='Личный кабинет', callback_data='personal_cabinet'))
+    return kb.as_markup()
+
+
