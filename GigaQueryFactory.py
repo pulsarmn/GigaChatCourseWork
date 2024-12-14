3c5bf8ba7f366981040fcf50e7b9748b2fe460f4
Кузичев Александр <pulsarmn@yandex.ru>
2024-12-14 14:45:32 +0300

    add: create GigaQueryFactory.py file

---
+++ GigaQueryFactory.py
@@ -0,0 +1,29 @@
+import random
+
+from langchain_gigachat.chat_models import GigaChat
+from langchain_core.messages import SystemMessage
+
+from config_data.config import Config, load_config
+
+config: Config = load_config()
+
+default_message = 'Ты — бот-генератор кода, напиши код и объяснение к нему. Если я прошу сгенерировать или рассказать информацию не связанную с генерацией кода или программированием, резко отказывайся и никогда не соглашайся рассказывать про тему, не связанную с программированием!!!'
+
+task_types = [
+    "функция для вычисления",
+    "программа для сортировки",
+    "алгоритм для поиска",
+    "программа для работы с"
+]
+
+operations = [
+    "факториала числа",
+    "чисел Фибоначчи",
+    "палиндрома",
+    "матрицами",
+    "очередью",
+    "стеком"
+]
+
+languages = ["C", "C++", "Java", "Rust", "Go", "JavaScript", "Python"]
+
