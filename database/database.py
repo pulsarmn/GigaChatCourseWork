0f81f6ff17251fb8e27c0f91288b4a555735972a
Кузичев Александр <pulsarmn@yandex.ru>
2024-12-14 10:36:42 +0300

    add: implement the init_db function in the database.py

--- database/database.py
+++ database/database.py
@@ -3,3 +3,12 @@ import sqlite3
 
 DB_PATH = "database.db"
 
+
+async def init_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute('''CREATE TABLE IF NOT EXISTS users (
+                                id INTEGER PRIMARY KEY,
+                                queries_left INTEGER DEFAULT 20,
+                                is_premium INTEGER DEFAULT 0
+                            )''')
+        await db.commit()
