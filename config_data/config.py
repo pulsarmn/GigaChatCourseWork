820819caf5940e865f2442013c620d1982325433
Кузичев Александр <pulsarmn@yandex.ru>
2024-12-14 09:43:41 +0300

    add: implement the load_config function to the config.py file

--- config_data/config.py
+++ config_data/config.py
@@ -12,3 +12,11 @@ class Config:
     payment_token: str
 
     gigachat_token: str
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')),
+                  gigachat_token=env('GIGACHAT_TOKEN'),
+                  payment_token=env('PAYMENT_TOKEN'))
